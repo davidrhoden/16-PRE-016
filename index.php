fc6c9b424bd3bf15665deae81e1c1c102640d168
David Rhoden <david@davidrhoden.com>
2016-04-06 17:04:04 -0500

    style menu

--- index.php
+++ index.php
@@ -6,24 +6,27 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Sanders\Wingo Video Samples</title>
         <link rel="stylesheet" href="css/style.css">
+        <link href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="js/scripts.js"></script>
     </head>
     <body>
     <div id="logo"><img src="images/swlogo.svg"></div>
     <div id="menu">
-    	<a href="/?video=alleyes">All Eyes On You</a> /
-    	<a href="/?video=seeingstars">Seeing Stars</a> /
-    	<a href="/?video=stronger">Stronger</a> /
-    	<a href="/?video=barnstorm">Barnstorm</a> /
-    	<a href="/?video=dancehall">Dance Hall</a> /
-    	<a href="/?video=mortgage">GECU Mortgages</a> /
-    	<a href="/?video=grad">Grad</a> /
-    	<a href="/?video=mission">Mission</a> /
-    	<a href="/?video=summerdrives">Summer Drives</a> /
-    	<a href="/?video=chihuahua">Chihuahua</a> /
-    	<a href="/?video=climber">Climber</a> /
-    	<a href="/?video=holidatcard">The Holiday Card</a> /
-    	<a href="/?video=vacation">Vacation</a> /
-    	<a href="/?video=youngguru">Young Guru</a> /
+    	<a href="?video=alleyes">All Eyes On You</a> /
+    	<a href="?video=seeingstars">Seeing Stars</a> /
+    	<a href="?video=stronger">Stronger</a> /
+    	<a href="?video=barnstorm">Barnstorm</a> /
+    	<a href="?video=dancehall">Dance Hall</a> /
+    	<a href="?video=mortgage">GECU Mortgages</a> /
+    	<a href="?video=grad">Grad</a> /
+    	<a href="?video=mission">Mission</a> /
+    	<a href="?video=summerdrives">Summer Drives</a> /
+    	<a href="?video=chihuahua">Chihuahua</a> /
+    	<a href="?video=climber">Climber</a> /
+    	<a href="?video=holidaycard">The Holiday Card</a> /
+    	<a href="?video=vacation">Vacation</a> /
+    	<a href="?video=youngguru">Young Guru</a> /
     </div>
 
     <div id="flex-container">
@@ -73,8 +76,11 @@ switch ($i) {
     case "youngguru":
         echo "YoungGuruTV.mov";
         break;
+    default:
+        echo "AT_T_All_Eyes_on_You_30_070615.mov";
+    break;
 }
-?>" autobuffer controls poster="/images/video.png"></video>
+?>" autobuffer controls></video>
 	</div>
     </body>
 </html>
